b68e477fa877b006774f0e2f04accb3fe8432d80
Ariel <arielsalomon583@gmail.com>
2023-12-19 21:10:26 +0200

    week3 day1

---
+++ week 3/day 1/script.js
@@ -0,0 +1,29 @@
+let thing = 'Hello world'
+
+
+//console.log(thing);
+
+let a = 5
+let b = true
+let c = a+20
+
+//console.log(b);
+//console.log(a+c);
+//console.log(a*c+(b+a-1));
+
+let longString = "This is a very long string which needs " +
+                 "to wrap across multiple lines because " +
+                 "otherwise, my code is unreadable.";
+
+console.log(longString.length)
+
+let str='a'
+
+let num=1000
+let f=num*3.2
+console.log(`${num}$ is worth ${f} nis `);
+
+
+
+
+
