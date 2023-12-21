1dd8e23524c34a151c043d20a233328081b167d0
Ariel <arielsalomon583@gmail.com>
2023-12-21 21:17:49 +0200

    week3 day5

---
+++ week 3/week3/day2.js
@@ -0,0 +1,53 @@
+let mydata = {
+    name: `ariel`,
+    lastname: `tagania`,
+    age:`18`,
+}
+ 
+mydata.dekete=`lastname`,
+mydata.eyecolor= `broun`, 
+
+console.log(mydata);
+
+let userdeta = {
+    username:`ariel`,
+    password:``
+
+    
+
+}
+
+let = myAge =25
+
+
+if (myAge > 18 && myage < 21) {
+console.log(`ariel can drink,not in the us`);
+}
+
+else {
+    console.log("ariel can`t drink");
+
+}
+
+
+let password = 107677896
+
+ if (password.longth > 18) {
+console.log(`password is too long`);
+}
+
+else if (passsword.longth > 8 && password.longth < 18 ) {
+    console.log(`password is ok`);
+}
+
+else{ (password.longth > 8 ) 
+    console.log(`password is too short`);
+}
+
+
+
+
+
+ 
+
+
