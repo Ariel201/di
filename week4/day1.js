642db2e88b483a255410b778c09b91a083238a2f
Ariel <arielsalomon583@gmail.com>
2023-12-26 21:07:57 +0200

    week4 day2

---
+++ week4/day1.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 2; i++) {
+    console.log("the current number is " + i)
+}
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
---
+++ week4/day2.js
@@ -0,0 +1,40 @@
+function sayhello (name, age, eyeColor=`brown`) {
+ console.log(`hello ${name}, are you ${age} years ago,
+ your eyes are ${eyeColor}`);
+}
+
+sayhello('chris',24)
+sayhello('david',)
+
+
+function change (usd) {
+    return usd*3.6;
+}
+//change(50)
+
+console.log(`50 dollar = ${change(50)}
+shekel`);
+
+function change2 (nis) {
+    console.log(nis*0.28)
+}
+change2(80)
+
+function familyAge (myAge) {
+    console.log(`my age is: ${myAge}. my mother's age is: ${myAge} my fofther's age is: ${myAge*2*1.2} `);
+}
+familyAge(25)
+
+
+function test () {
+    let mtage = 25
+    return myage
+}
+
+function motherage (myage) {
+    console.log(myage*2);
+}
+
+motherage (myAge())
+
+
