b4d8c9850db9bb9080b141dc9fe78477ef6b657e
Ariel <arielsalomon583@gmail.com>
2024-02-21 20:42:42 +0200

    week 5 day 4

--- exercises/ex.js
+++ exercises/ex.js
@@ -30,14 +30,50 @@
 //     console.log(pew + ' is an odd number');
 // }
 
-const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000",];
+// const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000",];
 
-if (users.length==0) {
-    console.log('no one is online');
-} else if (users.length==1) {
-    console.log(users[0],'is online');
-} else if (users.length==2) {
-console.log(users[0] , users[1]);
-}else{ 
-console.log(users[0],users[1],'and',users.length-2,'more online');
+// if (users.length==0) {
+//     console.log('no one is online');
+// } else if (users.length==1) {
+//     console.log(users[0],'is online');
+// } else if (users.length==2) {
+// console.log(users[0] , users[1]);
+// }else{ 
+// console.log(users[0],users[1],'and',users.length-2,'more online');
+// }
+
+// const people = ["Greg", "Mary", "Devon", "James"];
+// people.shift()
+// people[2]="Jason"
+// people.push("Ariel")
+// people.indexOf("Mary")
+// let bo = people.slice(1,3)
+// let p = people.indexOf("foo")
+ 
+// let last = people[people.length-1]
+// console.log(last);
+
+const eading1 = document.querySelector('h1')
+console.log(eading1);
+
+const eading2 = document.getElementById('lastP')
+eading2.remove();
+
+const eading3 = document.getElementById('h2')
+eading3.addEventListener('click',()=>{
+eading3.style.backgroundColor = "red";
+})
+
+const eading4 = document.getElementById('h3')
+eading4.addEventListener('click',()=>{
+eading4.style.display = 'none' 
+})
+
+const eading5 = document.getElementById('bold')
+eading5.addEventListener('click',()=>{
+const Ptags = document.getElementsByTagName('p')
+for (const Ptag of Ptags) {
+Ptag.style.fontWeight = "900";
+
 }
+})
--- week5/day3.js
+++ week5/day3.js
@@ -1,22 +1,39 @@
-// let button = document.getElementsByTagName('button')[0]
-// function raphy() {
-//     console.log('hello');
+// // let button = document.getElementsByTagName('button')[0]
+// // function raphy() {
+// //     console.log('hello');
+// // }
+
+
+// let ul= document.getElementById('shalom');
+// let chris= 1
+// function addline () {
+//    let li= document.createElement('li')
+//    li.textContent= `line number ${chris}`
+//    ul.appendChild(li) 
+//    chris++
 // }
 
+// let button2= document.getElementById('button')
+// button2.addEventListener('click',Respondmouseover)
 
-let ul= document.getElementById('shalom');
-let chris= 1
-function addline () {
-   let li= document.createElement('li')
-   li.textContent= `line number ${chris}`
-   ul.appendChild(li) 
-   chris++
+// let changeP= document.getElementById('onchange')
+// function e (params) {
+//     changeP.textContent = e.target.valou
+// }
+
+// let personform = document.forms[0]
+// let personform2 = document.forms.personform
+// let elements = personform.elements
+// let age = elements.age
+// let ageValue = age.value 
+// ageValue = 50
+// console.log(ageValue);
+
+function sayHello() {
+    alert('Hello!!!')
 }
-
-let button2= document.getElementById('button')
-button2.addEventListener('click',Respondmouseover)
-
-let changeP= document.getElementById('onchange')
-function e (params) {
-    changeP.textContent = e.target.valou
-}
+let cake = document.getElementById('cake')
+cake.addEventListener('click',sayHello)
+cake.addEventListener('click',()=>{
+console.log('cheesecake');
+})
