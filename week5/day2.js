c3d7aa1052abd4cac024a5ad728b6ea33ec1d113
Ariel <arielsalomon583@gmail.com>
2024-03-06 20:57:23 +0200

    week5 day2

--- week5/day2.js
+++ week5/day2.js
@@ -1,17 +1,17 @@
-const div1 = document.querySelector("div");
-console.log(div1);
-const list1 = document.getElementsByClassName("list")[0].lastElementChild;
-list1.textContent = "Richard";
-const list2 = document.getElementsByTagName("li")[3];
-list2.remove(list2);
-const ulLists = document.querySelectorAll(".list");
-console.log(ulLists);
+// const div1 = document.querySelector("div");
+// console.log(div1);
+// const list1 = document.getElementsByClassName("list")[0].lastElementChild;
+// list1.textContent = "Richard";
+// const list2 = document.getElementsByTagName("li")[3];
+// list2.remove(list2);
+// const ulLists = document.querySelectorAll(".list");
+// console.log(ulLists);
 
-for (let i = 0; i < ulLists.length; i++) {
-  const firstNameElement = ulLists[i].querySelector("li");
-  console.log(firstNameElement.textContent);
-  firstNameElement.textContent = "ariel"
-}
+// for (let i = 0; i < ulLists.length; i++) {
+//   const firstNameElement = ulLists[i].querySelector("li");
+//   console.log(firstNameElement.textContent);
+//   firstNameElement.textContent = "ariel"
+// }
 
 
 // const div = document.querySelector("div");
@@ -22,3 +22,28 @@ for (let i = 0; i < ulLists.length; i++) {
 // const pete = document.getElementsByTagName("li")[1];
 // pete.style.border = "solid black 5px";
 // pete.style.fontSize = "25px";
+   
+
+// let id = document.getElementById("navBar")
+// id.setAttribute("id","socialNetworkNavigation")
+// let li = document.createElement('li')
+// const textnode= document.createTextNode("Logout")
+// li.appendChild(textnode);
+// let ul = document.getElementsByTagName('ul')[0]
+// ul.appendChild(li)
+// let ul1 = document.getElementsByTagName('ul')[0]
+// let firstelement = ul.firstElementChild.textContent
+// let lastelement = ul.lastElementChild.textContent
+// console.log(lastelement);
+
+
+let allBooks = [{
+   title:'מייק אלדר שייטת 13 ' ,
+   auther:'מייק אלדר' ,
+   img:'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRF4L0ncREoigVZcgE4EPp2WgaUW4BGOfB1cpnFsvnHWOCBIYVM1g-55miJZXfce8E_EEcX_c1bUwf6qrfCO1H-Ennnm1u0JucFfUD124GHYqjX_Md4TLnW&usqp=CAc',
+   alreadyRead:false
+}, {
+  title:
+  auther:
+
+}]
